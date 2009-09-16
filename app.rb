7bd36d0cc62f4072d88a4ef6eea388b8f8b756d6
Eric Harris-Braun <eric@harris-braun.com>
2009-09-16 10:41:52 -0400

    converted over to using Net::HTTP instead of curl/wget and lots of interface improvements

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ require 'yaml'
 if File.exists?('config/config.rb')
   require 'config/config.rb'
 end
-CMD = '/usr/bin/curl' unless defined?(CMD)
+#CMD = '/usr/bin/curl' unless defined?(CMD)
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/db/zuptime.db")
 
 class Site
@@ -24,14 +24,22 @@ class Site
   property :status_changed, DateTime
   property :current_status, String
   property :last_result, Text
+  property :last_body, Text
   
   validates_is_unique :url
+  def down?
+    current_status == 'down'
+  end
 end
 
 class Configure
   include DataMapper::Resource
   property :id, Serial
   property :smtp_settings, Text
+  property :notify_from, String
+  property :retries, String, :default => 3
+  property :sleep, String, :default => 1
+  validates_is_number :retries,:sleep
 end
 
 Site.auto_upgrade!
@@ -61,7 +69,18 @@ post '/config/set' do
   @config = Configure.get(1)
   @config ||= Configure.new
   @config.smtp_settings = params[:smtp].to_yaml
-  @config.save ? redirect('/') : @config.errors.inspect
+  @config.notify_from = params[:notify_from]
+  @config.sleep = params[:sleep]
+  @config.retries = params[:retries]
+  if @config.save
+    redirect('/')
+  else
+    @smtp = @config.smtp_settings
+    @smtp = YAML.load(@smtp) if !@smtp.nil?
+    @smtp ||= {}
+    @error = @config.errors.inspect
+    haml :config
+  end
 end
 
 get '/' do
@@ -84,7 +103,20 @@ get '/sites/:id' do
   if @site
     haml :site_form
   else
-    'not found'
+    not_found_error
+  end
+end
+
+get '/sites/:id/last_body' do
+  @site = Site.get(params[:id])
+  if @site
+    @body = @site.last_body
+    @body = @body.gsub(/\&/,'&amp;')
+    @body = @body.gsub(/</,'&lt;')
+    @body = @body.gsub(/</,'&gt;')
+    haml :site_last_body
+  else
+    not_found_error
   end
 end
 
@@ -94,7 +126,7 @@ get '/sites/:id/check' do
     check_site(@site)
     redirect '/'
   else
-    'not found'
+    not_found_error
   end
 end
 
@@ -105,10 +137,11 @@ put '/sites' do
     if @site.save
       redirect '/'
     else
-      @site.errors.inspect
+      @error = @site.errors.inspect
+      haml :site_form
     end
   else
-    'not found'
+    not_found_error
   end
 end
 
@@ -118,7 +151,7 @@ delete '/sites' do
     @site.destroy
     redirect '/'
   else
-    'not found'
+    not_found_error
   end
 end
 
@@ -152,6 +185,13 @@ helpers do
       end
     end.join ''
   end
+  def not_found_error
+    render_error "id #{params[:id]} not found"
+  end
+  def render_error(error_text)
+    @error = error_text
+    haml :error
+  end
 end
 
 def check_sites
@@ -161,34 +201,74 @@ def check_sites
 end
 
 def check_site(site)
-  site.last_result = `#{CMD} #{site.url}`
   site.last_check = Time.now
-  if 
-    status = $? == 0 ? 'up' : 'down'
+  status = result = nil
+  (1..@config.retries.to_i).each do |x|
+    result = get_url(site.url)
+    status = is_down?(site,result) ? 'down' : 'up'
+    break if status == 'up'
+    sleep(@config.sleep.to_i)
+  end
+  if result.kind_of?(Net::HTTPResponse)
+    site.last_body = result.body
+    site.last_result = result.class
   else
-    status = 'down'
+    site.last_result = result.to_s
   end
   if site.current_status != status
     site.current_status = status
     site.status_changed = site.last_check
-    body = "Site Status Changed: #{status} (#{site.url})\n"
-    body += "\nstatus was #{$?}" if status == 'down'
-    pony_params = {
-      :from => 'zuptime@harris-braun.com',
-      :subject => "Site Status Changed: #{status} (#{site.url})",
-      :body => body
-      
-    }
-    @smtp = @config.smtp_settings
-    @smtp = YAML.load(@smtp) if !@smtp.nil?
-    @smtp.keys.each {|k| @smtp[k.to_sym] = @smtp[k]}
-    @smtp['auth'] = @smtp['auth'].to_sym if @smtp['auth']
-    if @smtp
-      pony_params[:via] = :smtp
-      pony_params[:smtp] = @smtp
+    notify_change(site)
+  end
+  raise "couldn't save" if !site.save
+end
+
+def get_url(uri)
+  require 'net/http'
+  uri = 'http://'+uri if uri !~ /^http:\/\//
+  uri = uri + '/' if uri !~ /\/$/
+  begin
+    url = URI.parse(uri)
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) do |http|
+      http.request(req)
     end
-    pony_params[:to] = site.notify
+    res
+  rescue Exception => e
+    e
+  end
+#  result = `#{CMD} #{site.url}`
+end
+
+def is_down?(site,result)
+  return false if result.kind_of?(Net::HTTPSuccess) || result.kind_of?(Net::HTTPRedirection)
+  true
+end
+
+def notify_change(site)
+  status = site.current_status
+  body = <<-EOTXT
+    zuptime reports that #{site.url} is now #{status}
+    
+    The response from the server was: #{site.last_result}
+    
+  EOTXT
+  pony_params = {
+    :from => @config.notify_from,
+    :subject => "zuptime: #{site.url} is #{status}",
+    :body => body
+    
+  }
+  @smtp = @config.smtp_settings
+  @smtp = YAML.load(@smtp) if !@smtp.nil?
+  @smtp.keys.each {|k| @smtp[k.to_sym] = @smtp[k]}
+  @smtp['auth'] = @smtp['auth'].to_sym if @smtp['auth']
+  if @smtp
+    pony_params[:via] = :smtp
+    pony_params[:smtp] = @smtp
+  end
+  pony_params[:to] = site.notify
+  if site.notify && site.notify != ''
     Pony.mail(pony_params)
   end
-  site.save
 end
